1ce448089f1f6485e8af0a8a9907bb3b32cc6859
Krzysztof Studniarek <studniak@amazon.lu>
2017-11-13 16:53:39 +0100

    Index.html with video tag

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from static import staticServingController
 def main():
     bottle.mount('show', videoServingController.app)
     bottle.mount('upload', videoUploadingController.app)
-    bottle.mount('static', staticServingController.app)
+    bottle.mount('static/', staticServingController.app)
     bottle.run(host='0.0.0.0', port=8080, debug=True)
 
 if __name__ == "__main__":
--- static/staticServingController.py
+++ static/staticServingController.py
@@ -2,7 +2,10 @@ from bottle import Bottle, route, static_file
 
 app = Bottle()
 
-#STATIC ROUTES
 @app.route('/<filename>')
 def serve_staic_files(filename):
-    return static_file(filename, root='static/')
+    return static_file(filename, root='/')
+
+@app.route('/videos/<filename>')
+def serve_videos(filename):
+    return static_file(filename, root='videos/')
