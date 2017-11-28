a49c8729b3969fa9eb6a014201ed6eb83a320791
Krzysztof Studniarek <studniak@amazon.lu>
2017-11-28 09:33:06 +0100

    video list endpoint

--- src/video_serving_controller.py
+++ src/video_serving_controller.py
@@ -13,18 +13,30 @@ def view_video_page():
     video_id = request.params['video_id']
     album_document = database_utils.get_album_document(album_id)
     
-    viewData = {
+    view_data = {
         'video_file_path' : album_document['videos'][str(video_id)]
     }
 
-    return template('video.html', viewData)
+    return template('video.html', view_data)
 
 @app.get('/albums')
 def view_album_list():
     album_documents = database_utils.get_all_album_documents()
 
-    viewData = {
+    view_data = {
         'albums' : map(lambda x: x['album_name'], album_documents)
     }
 
-    return template('albums.html', viewData)
+    return template('albums.html', view_data)
+
+@app.get('/videos')
+def view_videos_list():
+    album_id = request.params['album_id']
+    album_document = database_utils.get_album_document(album_id)
+
+    view_data = {
+        'album_name' : album_document['album_name'],
+        'videos' : album_document['videos']
+    }
+
+    return template('videos.html', view_data)
--- tst/tests_video_serving_controller.py
+++ tst/tests_video_serving_controller.py
@@ -12,6 +12,7 @@ from src import video_serving_controller
 test_album_id = '1123213'
 test_album_id_2 = '11232dasda'
 test_video_id = '123123'
+test_video_id_2 = '1dsadasf2'
 test_album_name = 'ala ma kota'
 test_album_name_2 = 'ala ma kota 2'
 test_video_file_path = 'sampleVideo.mp4'
@@ -21,14 +22,14 @@ test_album_list = [
             '_id' : test_album_id,
             'album_name' : test_album_name,
             'videos' : {
-                test_video_id : 'sampleVideo.mp4'
+                test_video_id : test_video_file_path
             }
         },
         {
             'album_id' : test_album_id_2,
             'album_name' : test_album_name_2,
             'videos' : {
-                test_video_id : 'sampleVideo.mp4'
+                test_video_id : test_video_file_path
             }
         }
     ]
@@ -39,13 +40,12 @@ def test_view_index():
 
 @mock.patch('src.utils.database_utils.get_album_document')
 def test_video_serivng(mocked_database_utils):
-    
     mocked_database_utils.return_value = {
             '_id' : test_album_id,
             'album_name' : test_album_name,
             'videos' : 
                 {
-                    test_video_id :'sampleVideo.mp4'
+                    test_video_id : test_video_file_path
                 }
         }
 
@@ -59,3 +59,18 @@ def test_get_list_of_albums(mocked_database_utils):
 
     with boddle(method='get'):
         assert video_serving_controller.view_album_list() == template('albums.html', {'albums' : [test_album_name, test_album_name_2]})
+
+@mock.patch('src.utils.database_utils.get_album_document')
+def test_get_list_of_videos(mocked_database_utils):
+    mocked_database_utils.return_value = {
+        '_id' : test_album_id,
+        'album_name' : test_album_name,
+        'videos' : 
+            {
+                test_video_id : test_video_file_path,
+                test_video_id_2 : test_video_file_path
+            }
+    }
+
+    with boddle(method='get', params={'album_id':test_album_id}):
+        assert video_serving_controller.view_videos_list() == template('videos.html', {'album_name' : test_album_name, 'videos' : { test_video_id : test_video_file_path, test_video_id_2 : test_video_file_path }})
