ecaa8fae67927bc42573ce2a976a942ae296d41a
Krzysztof Studniarek <studniak@amazon.lu>
2017-11-28 10:14:24 +0100

    tests for static files added

--- tst/tests_static_content_serving_controller.py
+++ tst/tests_static_content_serving_controller.py
@@ -1,17 +1,15 @@
 import pytest
 import sys, os
-from bottle import template
+from bottle import template, static_file
 from boddle import boddle
 
 sys.path.insert(0,
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from static import staic_content_serving_controller
+from static import static_content_serving_controller
 
-def testIndex():
-    with boddle():
-        assert videoServingController.viewIndexPage() == template('index.html')
+def test_static_files_serving():
+    assert static_content_serving_controller.serve_staic_files('style.css').status_code == 200
 
-def testVideoServingSite():
-    with boddle():
-        assert videoServingController.view_video_serve_page()
+def test_video_serving():
+    assert static_content_serving_controller.serve_videos('sampleVideo.mp4').status_code == 200
