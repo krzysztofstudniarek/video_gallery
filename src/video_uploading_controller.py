5d6c0b869b6e627dbdf1cb26fbe87984a87d6681
Krzysztof Studniarek <studniak@amazon.lu>
2017-11-29 12:42:42 +0100

    rename files and directories for pytest

--- src/video_uploading_controller.py
+++ src/video_uploading_controller.py
@@ -8,11 +8,15 @@ app = Bottle()
 def view_new_album_form():
     return template('newAlbum.html')
 
+@app.post('/new_album')
+def create_new_album():
+    return template('index.html')
+
 @app.get('/upload')
 def view_upload_video_form():
     return template('upload.html')
 
 @app.post('/upload')
-def upload_new_image(): 
+def upload_new_video(): 
     path = getcwd() + '/videos'
     return plupload.save(request.forms, request.files, path)
