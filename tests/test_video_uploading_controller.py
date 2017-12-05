3679aa8d01c989af9b3245873598fa8558a0d568
Krzysztof Studniarek <studniak@amazon.lu>
2017-12-05 13:23:04 +0100

    minor changes in uploading controller tess

--- tests/test_video_uploading_controller.py
+++ tests/test_video_uploading_controller.py
@@ -9,6 +9,10 @@ sys.path.insert(0,
 
 from src import video_uploading_controller
 
+test_ablum_id = '123213'
+test_album_name = 'test_album_name'
+test_album_doc_rev = 'sdas12312'
+
 def test_new_album_form_page():
     with boddle():
         view_data = {
@@ -17,13 +21,13 @@ def test_new_album_form_page():
         assert video_uploading_controller.view_new_album_form() == template('newAlbum.html', view_data)
 
 def test_video_upload_view():
-    with boddle(method='get', params={'album_id':'123213'}):
-        assert video_uploading_controller.view_upload_video_form() == template('upload.html', {'album_id' : '123213'})
+    with boddle(method='get', params={'album_id':test_ablum_id}):
+        assert video_uploading_controller.view_upload_video_form() == template('upload.html', {'album_id' : test_ablum_id})
 
 
 @mock.patch('src.utils.database_utils.save_album_document')
 def test_album_creation(mocked_database_utils):
-    mocked_database_utils.return_value = 'asd123131', 'sdas12312'
-    with boddle(method='post', params={'album_name':'test_album_name'}):
+    mocked_database_utils.return_value = test_ablum_id, test_album_doc_rev
+    with boddle(method='post', params={'album_name':test_album_name}):
         assert video_uploading_controller.create_new_album() == template('index.html')
         mocked_database_utils.assert_called_once()
