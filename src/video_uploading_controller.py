a8335642504f85c9c38cda55a406ed899cfbbbea
Krzysztof Studniarek <studniak@amazon.lu>
2017-12-06 14:36:31 +0100

    Bootstrap added

--- src/video_uploading_controller.py
+++ src/video_uploading_controller.py
@@ -14,8 +14,17 @@ def view_new_album_form():
 @app.post('/new_album')
 def create_new_album():
     album_name = _extract_album_name_from_request(request)
-    database_utils.save_album_document(album_name)
-    return template('index.html')
+    album_id, album_doc_rev = database_utils.save_album_document(album_name)
+    path = _get_videos_directory(album_id)
+    filesystem_utils.initailize_directory(path)
+
+    view_data = {
+        'album_name' : album_name,
+        'album_id' : album_id,
+        'videos' : filesystem_utils.get_videos_names(album_id)
+    }
+
+    return template('show_views/videos.html', view_data)
 
 @app.get('/upload')
 def view_upload_video_form():
@@ -27,9 +36,11 @@ def view_upload_video_form():
 
 @app.post('/upload')
 def upload_new_video(): 
-    path = getcwd() + '/static/videos/' + request.forms.get('album_id') + '/'
-    filesystem_utils.initailize_directory(path)
+    path = _get_videos_directory(request.forms.get('album_id'))
     return plupload.save(request.forms, request.files, path)
+
+def _get_videos_directory(album_id):
+    return getcwd() + '/static/videos/' + album_id + '/'
 
 def _extract_videos_from_request(request):
     return request.forms.getlist('videos[]')
--- static/scripts/uploader.js
+++ static/scripts/uploader.js
@@ -12,7 +12,7 @@ uploader.init();
 uploader.bind('FilesAdded', function(up, files) {
     var html = '';
     plupload.each(files, function(file) {
-        html += '<li id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></li>';
+        html += '<li class="list-group-item" id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></li>';
     });
     document.getElementById('filelist').innerHTML += html;
 });
